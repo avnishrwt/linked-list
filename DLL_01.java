3ce95df7e56158f20aa148e09c9d795ad9333bfd
avnishrwt <avnishrawat404@gmail.com>
2026-08-22 00:29:59 +0530

    doubly linked list initializing the node

---
+++ DLL_01.java
@@ -0,0 +1,15 @@
+// class creation of doubly linked list
+
+class node
+{
+    int data;
+    node next;
+    node back;
+
+    node(int data1 , node next1 , node back1)
+    {
+        this.data = data1;
+        this.next = next1;
+        back = back1;
+    }
+}
